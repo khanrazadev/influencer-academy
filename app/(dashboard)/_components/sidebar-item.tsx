74033e1b78d25ac7a872cf44b3301a5c0bb04a2d
Your Name <your.email@example.com>
2024-01-22 13:58:36 +0530

    change color of sidebar

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -30,19 +30,19 @@ const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
       className={cn(
         "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
         isActive &&
-          " text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
+          " text-fuchsia-700 bg-fuchsia-200/20 hover:bg-fuchsia-200/20 hover:text-fuchsia-700"
       )}
     >
       <div className="flex items-center gap-x-2 py-4">
         <Icon
           size={22}
-          className={cn("text-slate-500", isActive && "text-sky-700")}
+          className={cn("text-slate-500", isActive && "text-fuchsia-700")}
         />
         {label}
       </div>
       <div
         className={cn(
-          "ml-auto opacity-0 border-2 border-sky-700 h-full transition-all",
+          "ml-auto opacity-0 border-2 border-fuchsia-700 h-full transition-all",
           isActive && "opacity-100"
         )}
       />
