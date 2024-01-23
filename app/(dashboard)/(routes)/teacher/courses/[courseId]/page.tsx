42897a0e36b9e130a5742f5ce1a5db2def9a07c1
Your Name <your.email@example.com>
2024-01-23 18:58:29 +0530

    -Added uploadthing(S3) -Added middleware to upload on uploadthing -Added api to post & update (-Title,Description,Image) -Added Froms (title,descrption)

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,5 +1,11 @@
+import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
+import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+
+import TitleForm from "./_components/title-form";
+import DescriptionForm from "./_components/description-form";
+import ImageForm from "./_components/image-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const course = await db.course.findUnique({
@@ -30,6 +36,17 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
           <span className="text-sm text-slate-700">
             Complete all fields {completionText}
           </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">Customize your course</h2>
+          </div>
+          <TitleForm initialData={course} courseId={course.id} />
+          <DescriptionForm initialData={course} courseId={course.id} />
+          <ImageForm initialData={course} courseId={course.id} />
         </div>
       </div>
     </div>
