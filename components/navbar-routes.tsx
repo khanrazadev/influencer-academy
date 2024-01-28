a9757969a8221ca6582d7b770f7a344085fc3bce
Your Name <your.email@example.com>
2024-01-28 13:49:29 +0530

    Added courselist Added coursecard Added navbar-routes

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -4,6 +4,9 @@ import { UserButton, useAuth } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
 
 import { SearchInput } from "./search-input";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
 
 export const NavbarRoutes = () => {
   const { userId } = useAuth();
@@ -21,20 +24,20 @@ export const NavbarRoutes = () => {
         </div>
       )}
       <div className="flex gap-x-2 ml-auto">
-        {/* {isTeacherPage || isCoursePage ? (
+        {isTeacherPage || isCoursePage ? (
           <Link href="/">
             <Button size="sm" variant="ghost">
               <LogOut className="h-4 w-4 mr-2" />
               Exit
             </Button>
           </Link>
-        ) : isTeacher(userId) ? (
+        ) : (
           <Link href="/teacher/courses">
             <Button size="sm" variant="ghost">
               Teacher mode
             </Button>
           </Link>
-        ) : null} */}
+        )}
         <UserButton afterSignOutUrl="/" />
       </div>
     </>
