37807ba3d0c50d7248c79a1a40d7ab7f01cb89cd
Your Name <your.email@example.com>
2024-01-29 12:56:18 +0530

    Added course-progess

--- app/(course)/courses/[courseId]/_components/course-sidebar.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
 import { CourseSidebarItem } from "./course-sidebar-item";
+import { CourseProgress } from "@/components/course-progress";
 
 interface CourseSidebarProps {
   course: Course & {
@@ -39,7 +40,7 @@ export const CourseSidebar = async ({
         <h1 className="font-semibold">{course.title}</h1>
         {purchase && (
           <div className="mt-10">
-            {/* <CourseProgress variant="success" value={progressCount} /> */}
+            <CourseProgress variant="success" value={progressCount} />
           </div>
         )}
       </div>
