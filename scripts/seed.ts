891954b5d7c5a01fea5e87923d2d3c77ea715dc4
Raza Khan <63768854+khanrazadev@users.noreply.github.com>
2024-04-13 17:27:25 +0530

    Update seed.ts

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -6,10 +6,12 @@ async function main() {
   try {
     // Check if categories already exist in the database
     const existingCategories = await database.category.findMany();
-    
+
     // Filter out existing category names
-    const existingCategoryNames = existingCategories.map(category => category.name);
-    
+    const existingCategoryNames = existingCategories.map(
+      (category: { name: any }) => category.name
+    );
+
     // Define the categories you want to seed
     const categoriesToSeed = [
       { name: "Photography" },
@@ -20,15 +22,17 @@ async function main() {
       { name: "Travel" },
       { name: "Fashion" },
     ];
-    
+
     // Filter out categories that already exist in the database
-    const newCategories = categoriesToSeed.filter(category => !existingCategoryNames.includes(category.name));
-    
+    const newCategories = categoriesToSeed.filter(
+      (category) => !existingCategoryNames.includes(category.name)
+    );
+
     // Create only the new categories
     await database.category.createMany({
       data: newCategories,
     });
-    
+
     console.log("Categories seeded successfully.");
   } catch (error) {
     console.log("Error seeding the database categories.", error);
