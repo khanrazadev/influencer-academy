5e5f6082cc987a4923af8bc1b86f14491afab6ca
Raza Khan <63768854+khanrazadev@users.noreply.github.com>
2024-04-13 17:19:02 +0530

    Update seed.ts

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -4,17 +4,32 @@ const database = new PrismaClient();
 
 async function main() {
   try {
+    // Check if categories already exist in the database
+    const existingCategories = await database.category.findMany();
+    
+    // Filter out existing category names
+    const existingCategoryNames = existingCategories.map(category => category.name);
+    
+    // Define the categories you want to seed
+    const categoriesToSeed = [
+      { name: "Photography" },
+      { name: "Filming" },
+      { name: "Story Telling" },
+      { name: "Editing" },
+      { name: "Fitness" },
+      { name: "Travel" },
+      { name: "Fashion" },
+    ];
+    
+    // Filter out categories that already exist in the database
+    const newCategories = categoriesToSeed.filter(category => !existingCategoryNames.includes(category.name));
+    
+    // Create only the new categories
     await database.category.createMany({
-      data: [
-        { name: "Photography" },
-        { name: "Filming" },
-        { name: "Story Telling" },
-        { name: "Editing" },
-        { name: "Fitness" },
-        { name: "Travel" },
-        { name: "Fashion" },
-      ],
+      data: newCategories,
     });
+    
+    console.log("Categories seeded successfully.");
   } catch (error) {
     console.log("Error seeding the database categories.", error);
   } finally {
